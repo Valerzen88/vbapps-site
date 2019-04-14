de24a16a16d7cda6a0e1555c040fa137dd12b949
Valerzen88 <valeri.balachnin@gmail.com>
2019-04-14 12:32:47 +0200

    initial commit

---
+++ submit.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+if (isset($_POST["email_address"])) { 
+	ini_set('auto_detect_line_endings',true);
+	$text = file_get_contents("submits.ini");
+	if($text ==="") {
+		$text = $text.$_POST['email_address'];
+	} else {
+		$text = $text.";".$_POST['email_address'];
+	}
+	file_put_contents("submits.ini",$text);
+}
+header("Location: /index.html");
+?>
