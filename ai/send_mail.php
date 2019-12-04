948db13e6d07725a3b645e090a92464fb38aec35
Valerzen88 <valeri.balachnin@gmail.com>
2019-12-04 23:29:04 +0100

    fix emails

--- ai/send_mail.php
+++ ai/send_mail.php
@@ -4,6 +4,10 @@ setlocale(LC_MONETARY, 'de_DE');
 $link = "Location: http://vbapps.co/ai/";
 
 if (isset($_POST["send_inquiry"])) {
+	 $headers = 'From: payment@vbapps.co'."\r\n".'Reply-To: payment@vbapps.co' . "\r\n";
+	// To send HTML mail, the Content-type header must be set
+	$headers .= 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
     $message_text = "<u>Kundendaten:</u><br>
 	<ul>
 	<li>Vorname: " . $_POST['first_name'] . "</li>
@@ -14,10 +18,10 @@ if (isset($_POST["send_inquiry"])) {
 	<li>Nachricht: " . $_POST['lead_message'] . "</li>
 	</ul><br>";
 
-    mail($_POST['lead_mail'] . ", info@vbapps.co", "Neue Anfrage zu AMZ.Invest",
-        "Sehr geehrter Kunde,<br><br>Sie haben eine Anfrage zu AMZ.Invest mit folgendem Inhalt: <b>" .
+    mail($_POST['lead_email'] . ", info@vbapps.co", "Neue Anfrage zu AMZ.Invest",
+        "Sehr geehrter Kunde,<br><br>Sie haben eine Anfrage zu AMZ.Invest mit folgendem Inhalt: <b><br>" .
         $message_text . "</b> gesendet. <br><br>Das ist nur eine Kopie der Nachricht für Sie.
-        <br><br>Mit freundlichen Grüßen,<br><br>Ihr AMZ.Invest-Team<br><br>", $headers);
+        <br><br>Mit freundlichen Grüßen<br><br>Ihr AMZ.Invest-Team<br><br>", $headers);
     header($link);
 } else {
     header($link);
