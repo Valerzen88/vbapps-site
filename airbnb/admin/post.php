e94af9b7fd9ee83d0c7a693b35d9c500611bb1fe
Valeri Balachnin <valeri.balachnin@vbapps.co>
2020-01-19 20:22:58 +0100

    add html editor page fixed head tag

---
+++ airbnb/admin/post.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Reise eines AirBnB Hostes :: Blog über Gäste hosten & finanzielle Freiheit</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+    <script type="text/javascript">
+        //<![CDATA[
+        bkLib.onDomLoaded(function () {
+            new nicEditor({fullPanel: true}).panelInstance('blogpostarea');
+        });
+        //]]>
+    </script>
+</head>
+<body>
+<div id="editor">
+    <form action="post.php">
+        <label for="blogpostarea">Trage deinen Blogtext ein</label><textarea cols="200" rows="45" id="blogpostarea">
+Some Initial Content was in this textarea
+</textarea>
+        //TODO: specify other fields
+    </form>
+</div>
+<?php
+//post to self and save to db
+?>
+</body>
+</html>
