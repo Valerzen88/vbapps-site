382818b1412bdc60130762a1bf6918b7000827af
Valeri Balachnin <valeri.balachnin@vbapps.co>
2020-02-02 20:58:41 +0100

    add facebook btn optimise index.php

--- airbnb/index.php
+++ airbnb/index.php
@@ -1,93 +1,6 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <title>Reise eines Hosts :: Blog über Gäste hosten & finanzielle Freiheit</title>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link href="https://www.w3schools.com/w3css/4/w3.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <link href="images/apple-icon-57x57.png" rel="apple-touch-icon" sizes="57x57">
-    <link href="images/apple-icon-60x60.png" rel="apple-touch-icon" sizes="60x60">
-    <link href="images/apple-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
-    <link href="images/apple-icon-76x76.png" rel="apple-touch-icon" sizes="76x76">
-    <link href="images/apple-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
-<link href="images/apple-icon-120x120.png" rel="apple-touch-icon" sizes="120x120">
-<link href="images/apple-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
-<link href="images/apple-icon-152x152.png" rel="apple-touch-icon" sizes="152x152">
-<link href="images/apple-icon-180x180.png" rel="apple-touch-icon" sizes="180x180">
-<link href="images/android-icon-192x192.png" rel="icon" sizes="192x192" type="image/png">
-<link href="images/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
-<link href="images/favicon-96x96.png" rel="icon" sizes="96x96" type="image/png">
-<link href="images/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png">
-<link href="images/manifest.json" rel="manifest">
-<meta content="#ffffff" name="msapplication-TileColor">
-<meta content="images/ms-icon-144x144.png" name="msapplication-TileImage">
-<meta content="#ffffff" name="theme-color">
-<meta content="airbnb,reisen,gäste empfangen,hosten,gäste hosten,unternehmertum,shulden abbauen,investieren,immobillien,
-finanzielle freiheit,passives einkommen" name="keywords">
-<meta content="index,follow,max-image-preview:[large],max-snippet:[200]" name="robots">
-<meta content="Blog über das Hosten der Gäste und Skalierung der touristischen Vermietung als Business,
-welches dir ein unbeschwertes Leben möglich machen wird." name="description">
-<meta content="Valeri Balachnin" name="author">
-<!-- Open Graph / Facebook -->
-<meta content="website" property="og:type">
-<meta content="http://reise-eines-hosts.de/" property="og:url">
-    <meta content="Reise eines Hosts :: Blog über Gäste hosten" property="og:title">
-<meta content="Blog über das Hosten der Gäste und Skalierung der touristischen Vermietung als Business,
-welches dir ein unbeschwertes Leben möglich machen wird." property="og:description">
-<meta content="http://reise-eines-hosts.de/images/fb_airbnb_blog.png" property="og:image">
-
-    <!-- Twitter -->
-<meta content="summary_large_image" property="twitter:card">
-<meta content="http://reise-eines-hosts.de/" property="twitter:url">
-    <meta content="Reise eines Hosts :: Blog über Gäste hosten" property="twitter:title">
-<meta content="Blog über das Hosten der Gäste und Skalierung der touristischen Vermietung als Business,
-welches dir ein unbeschwertes Leben möglich machen wird." property="twitter:description">
-<meta content="http://reise-eines-hosts.de/images/fb_airbnb_blog.png" property="twitter:image">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155283849-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-
-    gtag('js', new Date());
-
-    gtag('config', 'UA-155283849-1');
-</script>
-<!-- Google Tag Manager -->
-<script>(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-        'gtm.start':
-            new Date().getTime(), event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-TBWCN33');</script>
-    <!-- End Google Tag Manager -->
-    <style>
-        body, h1, h2, h3, h4, h5 {
-            font-family: "Raleway", sans-serif
-        }
-    </style>
-</head>
-<body class="w3-light-grey">
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-    <iframe height="0"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TBWCN33" style="display:none;visibility:hidden"
-            width="0"></iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
-<!-- w3-content defines a container for fixed size centered content, 
-and is wrapped around the whole page content, except for the footer in this example -->
+<?php include('header_top.html'); ?>
+<title>Reise eines Hosts :: Blog über Gäste hosten & finanzielle Freiheit</title>
+<?php include('header.html') ?>
 <div class="w3-content" style="max-width:1400px">
 
     <!-- Header -->
@@ -101,67 +14,36 @@ and is wrapped around the whole page content, except for the footer in this exam
 
         <!-- Blog entries -->
         <div class="w3-col l8 s12">
+
             <!-- Blog entry -->
             <div class="w3-card-4 w3-margin w3-white">
-                <img src="images/blog_real_estate.png" style="width:100%;">
+                <center><img src="images/schranken_im_kopf.jpg" style="width:100%;height:100%;"></center>
                 <div class="w3-container">
-                    <h3><b>Wie ich zum Gastgeben kam...</b></h3>
-                    <h5>von Valerian, <span class="w3-opacity">12. Dezember 2019</span></h5>
+                    <h3><b>Angst ist ein schlechter Berater!</b></h3>
+                    <h5>von Valerian, <span class="w3-opacity">25. Januar 2020</span></h5>
                 </div>
 
                 <div class="w3-container">
-                    <p>AAuf der Suche nach einem ständigen Einkommen mit wenig Arbeitsaufwand bin ich auf die Idee
-                        gekommen
-                        ein Zimmer in der Wohnung in der ich damals gewohnt habe für Touristen zu vermieten. Das
-                        schwierigste war für mich dabei ...</p>
+                    <p>Angst ist ein schlechter Berater. Diesen Satz haben wir alle oft gehört, doch, was
+                        steckt hinter
+                        diesem Satz und was hat es mit der finanziellen Freiheit zu tun? Die Antwort ist: Alles!</p>
                     <div class="w3-row">
                         <div class="w3-col m8 s12">
                             <p>
                                 <button class="w3-button w3-padding-large w3-white w3-border">
-                                    <b><a href="1.html"> WEITERLESEN »</a></b>
-                                </button>
-                            </p>
-                        </div>
-                        <div class="w3-col m4 w3-hide-small">
-                            <p><span class="w3-padding-large w3-right"
-                                     style="visibility: hidden;"><b>Kommentare&nbsp;</b><span
-                                    class="w3-tag">0</span></span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-
-            <!-- Blog entry -->
-            <div class="w3-card-4 w3-margin w3-white">
-                <center><img src="images/increase_property_value.png" style="width:55%;height:55%;"></center>
-                <div class="w3-container">
-                    <h3><b>Mehrere Properties aufbauen</b></h3>
-                    <h5>von Valerian, <span class="w3-opacity">14. Dezember 2019</span></h5>
-                </div>
-
-                <div class="w3-container">
-                    <p>Das wichtigste bei einem Unternehmen ist, dass man das Fundament gelegt hat. Danach ist es, das
-                        ganze bis ins Unendliche zu wiederholen:SKALIEREN. Wie habe ich es anhand der Vermietung
-                        geschafft?</p>
-                    <div class="w3-row">
-                        <div class="w3-col m8 s12">
-                            <p>
-                                <button class="w3-button w3-padding-large w3-white w3-border">
-                                    <b><a href="2.html"> WEITERLESEN »</a></b>
+                                    <b><a href="4.html"> WEITERLESEN »</a></b>
                                 </button>
                             </p>
                         </div>
                         <div class="w3-col m4 w3-hide-small">
                             <p><span class="w3-padding-large w3-right"
                                      style="visibility: hidden;"><b>Comments  </b> <span
-                                    class="w3-badge">2</span></span>
+                                            class="w3-badge">2</span></span>
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
-
             <!-- Blog entry -->
             <div class="w3-card-4 w3-margin w3-white">
                 <center><img src="images/human_pyramide.png" style="width:55%;height:55%;"></center>
@@ -193,21 +75,21 @@ and is wrapped around the whole page content, except for the footer in this exam
             </div>
             <!-- Blog entry -->
             <div class="w3-card-4 w3-margin w3-white">
-                <center><img src="images/schranken_im_kopf.jpg" style="width:55%;height:55%;"></center>
+                <center><img src="images/increase_property_value.png" style="width:55%;height:55%;"></center>
                 <div class="w3-container">
-                    <h3><b>Angst ist ein schlechter Berater!</b></h3>
-                    <h5>von Valerian, <span class="w3-opacity">25. Januar 2020</span></h5>
+                    <h3><b>Mehrere Properties aufbauen</b></h3>
+                    <h5>von Valerian, <span class="w3-opacity">14. Dezember 2019</span></h5>
                 </div>
 
                 <div class="w3-container">
-                    <p>Angst ist ein schlechter Berater. Diesen Satz haben wir alle oft gehört, doch, was
-                        steckt hinter
-                        diesem Satz und was hat es mit der finanziellen Freiheit zu tun? Die Antwort ist: Alles!</p>
+                    <p>Das wichtigste bei einem Unternehmen ist, dass man das Fundament gelegt hat. Danach ist es, das
+                        ganze bis ins Unendliche zu wiederholen:SKALIEREN. Wie habe ich es anhand der Vermietung
+                        geschafft?</p>
                     <div class="w3-row">
                         <div class="w3-col m8 s12">
                             <p>
                                 <button class="w3-button w3-padding-large w3-white w3-border">
-                                    <b><a href="4.html"> WEITERLESEN »</a></b>
+                                    <b><a href="2.html"> WEITERLESEN »</a></b>
                                 </button>
                             </p>
                         </div>
@@ -220,13 +102,43 @@ and is wrapped around the whole page content, except for the footer in this exam
                     </div>
                 </div>
             </div>
+            <!-- Blog entry -->
+            <div class="w3-card-4 w3-margin w3-white">
+                <img src="images/blog_real_estate.png" style="width:100%;">
+                <div class="w3-container">
+                    <h3><b>Wie ich zum Gastgeben kam...</b></h3>
+                    <h5>von Valerian, <span class="w3-opacity">12. Dezember 2019</span></h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Auf der Suche nach einem ständigen Einkommen mit wenig Arbeitsaufwand bin ich auf die Idee
+                        gekommen
+                        ein Zimmer in der Wohnung in der ich damals gewohnt habe für Touristen zu vermieten. Das
+                        schwierigste war für mich dabei ...</p>
+                    <div class="w3-row">
+                        <div class="w3-col m8 s12">
+                            <p>
+                                <button class="w3-button w3-padding-large w3-white w3-border">
+                                    <b><a href="1.html"> WEITERLESEN »</a></b>
+                                </button>
+                            </p>
+                        </div>
+                        <div class="w3-col m4 w3-hide-small">
+                            <p><span class="w3-padding-large w3-right"
+                                     style="visibility: hidden;"><b>Kommentare&nbsp;</b><span
+                                            class="w3-tag">0</span></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <!-- END BLOG ENTRIES -->
             <div class="w3-card-4 w3-margin w3-white">
                 <div class="w3-container">
                     <p align="center" style="background-color:transparent;">
                         <button class="w3-button w3-padding-large w3-border" id="btn_to_airbnb"
                                 onclick="window.location.href = 'https://www.airbnb.de/r/valerib126?s=6&t=061n';"><b>AUCH
-                            GASTGEBER WERDEN!</b></button>
+                                GASTGEBER WERDEN!</b></button>
                         <br><br>Registriere Dich noch heute!
                         Es lohnt sich auf jeden Fall! Bau dir einen beständigen Einkommen auf!
                     </p>
@@ -234,71 +146,38 @@ and is wrapped around the whole page content, except for the footer in this exam
             </div>
         </div>
 
-        <!-- Introduction menu -->
-        <div class="w3-col l4">
-            <!-- About Card -->
-            <div class="w3-card w3-margin w3-margin-top">
-                <img alt="" src="images/me_blog.png" style="width:100%;">
-                <div class="w3-container w3-white">
-                    <h4><b>Valerian</b></h4>
-                    <p align="center">
-                        Seit 2018 vermiete automatisiert verschiedene Objekte über AirBnB und Booking.
-                        Ich möchte meine Erfahrung mit anderen teilen und anderen von möglichen Fehlern zu bewahren.<br>
-                        Willst du mehr über automatisierte Objektvermietung erfahren? <br>Frag mich! -> <a
-                            href="mailto:info@reise-eines-hosts.de">E-Mail senden</a>
-                    </p>
-                </div>
-            </div>
-            <hr>
 
-            <div class="w3-card w3-margin">
-                <div class="w3-container w3-padding">
-                    <h4>Gastgeben</h4>
-                    <div class="w3-container w3-white">
-                        <p align="center">Registriere Dich noch heute! <br>Es lohnt sich auf jeden Fall! <br>Bau dir
-                            einen beständigen Einkommen auf!</p>
-                        <div align="center">
-                            <button class="w3-button w3-padding-large w3-border"
-                                    onclick="window.location.href = 'https://www.airbnb.de/r/valerib126?s=6&t=061n';this.target='_blank';">
-                                <b>AUCH GASTGEBER WERDEN!</b></button>
-                        </div>
-                    </div>
-                </div>
+        <?php include('right_side.html'); ?>
+        <!-- Posts -->
+        <!--<div class="w3-card w3-margin" style="visibility:hidden;">
+            <div class="w3-container w3-padding">
+                <h4>Popular Posts</h4>
             </div>
-            <!-- Posts -->
-            <!--<div class="w3-card w3-margin" style="visibility:hidden;">
-                <div class="w3-container w3-padding">
-                    <h4>Popular Posts</h4>
-                </div>
-                <ul class="w3-ul w3-hoverable w3-white">
-                    <li class="w3-padding-16">
-                        <img alt="Image" class="w3-left w3-margin-right" src="/w3images/workshop.jpg"
-                             style="width:50px">
-                        <span class="w3-large">Lorem</span><br>
-                        <span>Sed mattis nunc</span>
-                    </li>
-                    <li class="w3-padding-16">
-                        <img alt="Image" class="w3-left w3-margin-right" src="/w3images/gondol.jpg" style="width:50px">
-                        <span class="w3-large">Ipsum</span><br>
-                        <span>Praes tinci sed</span>
-                    </li>
-                    <li class="w3-padding-16">
-                        <img alt="Image" class="w3-left w3-margin-right" src="/w3images/skies.jpg" style="width:50px">
-                        <span class="w3-large">Dorum</span><br>
-                        <span>Ultricies congue</span>
-                    </li>
-                    <li class="w3-padding-16 w3-hide-medium w3-hide-small">
-                        <img alt="Image" class="w3-left w3-margin-right" src="/w3images/rock.jpg" style="width:50px">
-                        <span class="w3-large">Mingsum</span><br>
-                        <span>Lorem ipsum dipsum</span>
-                    </li>
-                </ul>
-            </div>-->
+            <ul class="w3-ul w3-hoverable w3-white">
+                <li class="w3-padding-16">
+                    <img alt="Image" class="w3-left w3-margin-right" src="/w3images/workshop.jpg"
+                         style="width:50px">
+                    <span class="w3-large">Lorem</span><br>
+                    <span>Sed mattis nunc</span>
+                </li>
+                <li class="w3-padding-16">
+                    <img alt="Image" class="w3-left w3-margin-right" src="/w3images/gondol.jpg" style="width:50px">
+                    <span class="w3-large">Ipsum</span><br>
+                    <span>Praes tinci sed</span>
+                </li>
+                <li class="w3-padding-16">
+                    <img alt="Image" class="w3-left w3-margin-right" src="/w3images/skies.jpg" style="width:50px">
+                    <span class="w3-large">Dorum</span><br>
+                    <span>Ultricies congue</span>
+                </li>
+                <li class="w3-padding-16 w3-hide-medium w3-hide-small">
+                    <img alt="Image" class="w3-left w3-margin-right" src="/w3images/rock.jpg" style="width:50px">
+                    <span class="w3-large">Mingsum</span><br>
+                    <span>Lorem ipsum dipsum</span>
+                </li>
+            </ul>
+        </div>-->
             <hr>
-            <a href="https://twitter.com/HostsReise?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large"
-               data-show-screen-name="false" data-lang="de" data-dnt="true" data-show-count="false">Follow
-                @HostsReise</a>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             <!-- Labels / tags -->
             <div class="w3-card w3-margin" style="visibility:hidden;">
                 <div class="w3-container w3-padding">
@@ -333,3 +212,5 @@ and is wrapped around the whole page content, except for the footer in this exam
     <!-- END w3-content -->
 </div>
 <?php include('footer.html'); ?>
+</body>
+</html>
